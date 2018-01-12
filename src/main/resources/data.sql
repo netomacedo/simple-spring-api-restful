6190c92940d1f10804c4178c2e66f0c2a7a2b15b
Francisco Assis Macêdo Neto <netomacedo.20@gmail.com>
2018-01-12 16:21:51 +0000

    Sql file
    INSERT SQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO public.vehicle(registration_number, colour, model, registration_date, status)
+	VALUES (1, 'blue', 'bmw', '2018-01-01', '1');
+    
+INSERT INTO public.vehicle(registration_number, colour, model, registration_date, status)
+	VALUES (2, 'green', 'volvo', '2018-01-02', '2');
+    
+INSERT INTO public.vehicle(registration_number, colour, model, registration_date, status)
+	VALUES (3, 'black', 'fiat', '2018-01-03', '2');
+    
+INSERT INTO public.vehicle(registration_number, colour, model, registration_date, status)
+	VALUES (4, 'white', 'kia', '2018-01-04', '1');
+    
+INSERT INTO public.vehicle(registration_number, colour, model, registration_date, status)
+	VALUES (5, 'purple', 'honda', '2018-01-05', '1');
+    
+INSERT INTO public.vehicle(registration_number, colour, model, registration_date, status)
+	VALUES (6, 'brown', 'vw', '2018-01-06', '2');
+    
+    
